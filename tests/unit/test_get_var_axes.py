6a67c6bc98e4ad32dbff755b98fe6d3e20d52f27
Matt Field <matthew.field@ukaea.uk>
2025-09-25 17:29:37 +0100

    Added first test for get_var_axes

---
+++ tests/unit/test_get_var_axes.py
@@ -0,0 +1,39 @@
+from simvue_remkit.connector import RemkitRun
+import simvue
+import threading
+import time
+import tempfile
+from unittest.mock import patch
+import uuid
+import pathlib
+import pytest
+import json
+
+from RMK_support.IO_support import loadFromHDF5
+from RMK_support.grid import gridFromDict, Grid
+
+
+def test_get_var_axes_2d():
+    with open(pathlib.Path(__file__).parent.joinpath("example_data", "RMK_advection_test", "config.json"), "r") as config_file:
+        config = json.load(config_file)
+    var_list = ["time", "n", "n_dual", "G", "G_dual"]
+    grid = gridFromDict(config)
+    data = loadFromHDF5(grid, var_list, [str(pathlib.Path(__file__).parent.joinpath("example_data", "RMK_advection_test","ReMKiT1DVarOutput_0.h5"))])
+    with RemkitRun() as run:
+        run.dual_vars = ["n_dual", "G_dual"]
+        run._get_var_axes(data.dataset)
+    
+    # Check n_dual and G_dual have axes as 'x_dual'
+    assert run._var_coords["n_dual"]["axes"] == ["x_dual"]
+    assert run._var_coords["G_dual"]["axes"] == ["x_dual"]
+    
+    # Check time has no axes (1D metric)
+    assert run._var_coords["time"]["axes"] == []
+    
+    # Check others have axes as 'x'
+    assert run._var_coords["n"]["axes"] == ["x"]
+    assert run._var_coords["G"]["axes"] == ["x"]
+
+    # Check all have no harmonics
+    for var in var_list:
+        assert run._var_coords[var]["harmonics"] is None
