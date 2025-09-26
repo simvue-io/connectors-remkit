4b8fff0f33a7415fd720b617b808056356e57ce9
Matt Field <matthew.field@ukaea.uk>
2025-09-26 10:02:54 +0100

    Added 3D get_axes test

--- tests/unit/test_get_var_axes.py
+++ tests/unit/test_get_var_axes.py
@@ -37,3 +37,22 @@ def test_get_var_axes_2d():
     # Check all have no harmonics
     for var in var_list:
         assert run._var_coords[var]["harmonics"] is None
+
+def test_get_var_axes_3d():
+    with open(pathlib.Path(__file__).parent.joinpath("example_data", "RMK_kin_adv_test", "config.json"), "r") as config_file:
+        config = json.load(config_file)
+    var_list = ["time", "f"]
+    grid = gridFromDict(config)
+    data = loadFromHDF5(grid, var_list, [str(pathlib.Path(__file__).parent.joinpath("example_data", "RMK_kin_adv_test","ReMKiT1DVarOutput_0.h5"))])
+    with RemkitRun() as run:
+        run.dual_vars = []
+        run._get_var_axes(data.dataset)
+    
+    # Check f has both axes as 'x', 'v'
+    assert run._var_coords["f"]["axes"] == ["x", "v"]
+    
+    # Check time has no axes (1D metric)
+    assert run._var_coords["time"]["axes"] == []
+    
+    # Check f has harmonics, 0 and 1
+    assert run._var_coords["f"]["harmonics"] == [0,1]
